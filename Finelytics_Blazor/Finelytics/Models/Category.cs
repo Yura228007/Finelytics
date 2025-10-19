8b47a4e3e28108753f7c649413bb017bce9ea77d
Yura Schedrin <iuriishchedrin@gmail.com>
2025-10-19 21:10:06 +0300

    Add Models to Blazor

---
+++ Finelytics_Blazor/Finelytics/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Finelytics.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal PlannedAmmount { get; set; }
+        public bool IsIncome { get; set; }
+    }
+}
