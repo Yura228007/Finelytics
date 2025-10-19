0ed8989c6a3a7e9dfc44b05e611cf8c3fffbfa30
Yura Schedrin <iuriishchedrin@gmail.com>
2025-10-19 21:20:07 +0300

    Removing

--- Finelytics/Finelytics/Models/Group.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Finelytics.Models
-{
-    public class Group
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal PlannedAmmount { get; set; }
-    }
-}
